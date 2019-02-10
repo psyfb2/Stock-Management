41227cf9c0558ab54b78ef11a2c6389e0d7e26ff
psyfb2 <psyfb2@exmail.nottingham.ac.uk>
2019-02-10 02:20:21 +0000

    psyfb2: PULL ME BEFORE YOU GET STARTED!

--- project/src/com/g52grp/database/DatabaseConnection.java
+++ project/src/com/g52grp/database/DatabaseConnection.java
@@ -31,16 +31,30 @@ public class DatabaseConnection {
 	
 	/**
 	 * 
-	 * @param sql SQL command to send to the mysql server
+	 * @param sql SQL query to be sent to the database, typically a SQL SELECT statement
 	 * @return ResultSet for the query, null if query was unsuccessful 
 	 */
-	public ResultSet sendSql(String sql) {
+	public ResultSet sendQuery(String sql) {
 		try {
 			Statement s = con.createStatement();
 			return s.executeQuery(sql);
 		} catch(Exception e) {
 			return null;
 		}
+	}
+	
+	/**
+	 * @param sql SQL Data Manipulation Language (DML) statement, such as INSERT, UPDATE or DELETE; or an SQL statement that returns nothing, such as a DDL statement.
+	 * @return whether the sql statement was executed by the database successfully
+	 */
+	public boolean sendUpdate(String sql) {
+		try {
+			Statement s = con.createStatement();
+			s.executeUpdate(sql);
+		} catch(Exception e) {
+			return false;
+		}
+		return true;
 	}
 	
 	/**
